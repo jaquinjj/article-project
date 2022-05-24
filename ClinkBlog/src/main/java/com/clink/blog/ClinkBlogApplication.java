daadef4886a361b67e3c46a246fb7d3bc75daa6c
M.Mert <M.Mert@DESKTOP-J06RA52>
2022-05-24 20:42:12 +0200

    Init Commit

---
+++ ClinkBlog/src/main/java/com/clink/blog/ClinkBlogApplication.java
@@ -0,0 +1,13 @@
+package com.clink.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinkBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinkBlogApplication.class, args);
+	}
+
+}
---
+++ ClinkBlog/src/test/java/com/clink/blog/ClinkBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clink.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinkBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
